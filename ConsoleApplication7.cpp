60724336f565f443f53ba313ec0e301eb885caa8
amigo228 <61027311+amigo228@users.noreply.github.com>
2021-11-12 16:43:57 +0200

    Create ConsoleApplication7.cpp

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,21 @@
+﻿// ConsoleApplication7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	int s = 0;
+	cin >> a >> b;
+	for (int i = a; i <= b; ++i)
+	{
+		s++;
+		cout << i << " ";
+	}
+	cout <<endl<< s;
+}
+
+

f6c03efe0d108d9b95907b5d2f5bd60fb40f6f50
amigo228 <61027311+amigo228@users.noreply.github.com>
2021-11-12 16:25:00 +0200

    Create ConsoleApplication6.cpp

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	unsigned n,k;
+	cin >> n;
+	k = n;
+	while ((k*k)>n)
+	{
+		--k;
+	}
+	cout << k;
+}
+
+
+
+

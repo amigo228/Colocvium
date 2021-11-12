6a60cd3b4f173c4c511a089ce0e69631c0e31154
amigo228 <61027311+amigo228@users.noreply.github.com>
2021-11-12 16:51:48 +0200

    Create ConsoleApplication8.cpp

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	short day;
+	cin >> day;
+	switch (day)
+	{
+	case 1:
+		cout << "Понеділок"; break;
+	case 2:
+		cout << "Вівторок"; break;
+	case 3:
+		cout << "Середа"; break;
+	case 4:
+		cout << "Четвер"; break;
+	case 5:
+		cout << "П'ятниця"; break;
+	case 6:
+		cout << "Субота"; break;
+	case 7:
+		cout << "Неділя"; break;
+	default:
+		cout << "Error";
+		break;
+	}
+}
+
+
